cb1d6a8da6052cb3379039d5425ec3e3e5a953fd
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-11-04 00:46:11 +0530

    updated

--- kora/src/App.js
+++ kora/src/App.js
@@ -1,20 +1,23 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Error from "./pages/Error";
 
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <Router>
         <Header />
         <Routes>
           <Route path="/" element={<Landing />} exact />
+          <Route path="*" element={<Error />} exact />
+          {/* TODO: make these links accessible only if the user is logged in */}
           <Route path="/home" element={<Landing />} exact />
         </Routes>
         <Footer />
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
