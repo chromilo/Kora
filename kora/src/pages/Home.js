b1543d128344920afb10fd28df3e6834f0fbd485
Abisoye Onanuga <102636953+dottymatrix@users.noreply.github.com>
2023-11-04 11:36:07 -0400

    yarn install

--- kora/src/pages/Home.js
+++ kora/src/pages/Home.js
@@ -1,8 +1,33 @@
 import React from 'react'
 
-const Home = () => {
-  return (
-    <div>Home</div>
+const Home = () => 
+{
+  return 
+  (
+    <html>
+          <section class="left-sidebar" >
+            <section>
+              <h1>Kora</h1>
+              
+            </section>
+          </section>
+
+          <section class="top-panel">
+            <section class="np-panel">
+              <h2>Now Playing</h2>
+            </section>
+            <section class="q-panel">
+              <h2>Queue</h2>
+            </section>
+          </section><br>
+
+          <section class="main-panel">
+            <section class="thumbnail">
+              <h3>POP</h3>
+            </section>
+          </section>
+    </html>
+
   )
 }
 
